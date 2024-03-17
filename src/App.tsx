b33bea419402c086c2afca9e17919e83f42506ae
smgsankar <gomzsankar@gmail.com>
2024-03-18 04:13:12 +0530

    fix: initialize firebase app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import "./styles/App.css";
+import { useEffect } from "react";
+import { initializeApp } from "firebase/app";
 import { Topbar } from "./sections/Topbar";
 import { Home } from "./sections/Home";
 import { BackdropImage } from "./components/BackdropImage";
+import "./styles/App.css";
 // import { Contact } from "./sections/Contact";
 // import { Footer } from "./sections/Footer";
 // import { Gallery } from "./sections/Gallery";
@@ -9,6 +11,10 @@ import { BackdropImage } from "./components/BackdropImage";
 // import { About } from "./sections/About";
 
 function App() {
+  useEffect(() => {
+    initializeApp(JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG || "{}"));
+  });
+
   return (
     <main id="main">
       <div className="homeWithNav">
