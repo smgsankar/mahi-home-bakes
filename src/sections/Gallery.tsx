dff7e9963a8b80f16d9e7b2f0068533dfd4c1fb9
smgsankar <gomzsankar@gmail.com>
2024-04-15 00:18:51 +0530

    fix: add basic layout for gallery

--- src/sections/Gallery.tsx
+++ src/sections/Gallery.tsx
@@ -1,3 +1,23 @@
+import { PrimaryButton } from "../components/PrimaryButton";
+import "../styles/gallery.css";
+
 export const Gallery = () => {
-  return <section id="gallery">Gallery</section>;
+  return (
+    <section id="gallery">
+      <div className="gallery-content-container">
+        <h5>
+          From our oven to your table, here are a few snaps from the homemade
+          delights that tell a story!
+        </h5>
+        <PrimaryButton onClick={() => console.log("View gallery")}>
+          View Gallery
+        </PrimaryButton>
+      </div>
+      <img
+        id="showcase"
+        src="gallery-showcase.png"
+        alt="Mahi home bakes showcase"
+      />
+    </section>
+  );
 };
