2ae5780f2aeb833a1628cdba88f42143334bb392
smgsankar <gomzsankar@gmail.com>
2025-06-21 13:25:39 +0530

    rename file

--- src/components/sections/contact-section.tsx
+++ src/components/sections/contact-section.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { emailAddress, whatsappLink } from "@/data/content";
-import { MessageSquare } from "../icons/message";
+import { MessageSquare } from "../icons/message-square";
 import { MapPin } from "../icons/map-pin";
 import { Mail } from "../icons/mail";
 
